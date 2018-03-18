41cd7697d1c3316852734a4bd4a8133efcbee84c
FrancescoPetro <37444709+FrancescoPetro@users.noreply.github.com>
2018-03-18 16:28:18 +0100

    User profile + Edit profile

--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
@@ -1,13 +1,33 @@
 package it.polito.mad.koko.lab1.kokolab1;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ShowProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_show_profile);
+
+        Button button=findViewById(R.id.edit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),EditProfile.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
+
 }
