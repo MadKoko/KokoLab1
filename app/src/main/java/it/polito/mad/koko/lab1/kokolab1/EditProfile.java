8ab92217b7675e51912e3cbf86de3abff51d5db7
FrancescoPetro <37444709+FrancescoPetro@users.noreply.github.com>
2018-03-18 19:07:46 +0100

    Commit 18/03/18

--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/EditProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/EditProfile.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity{
 
@@ -21,16 +23,38 @@ public class EditProfile extends AppCompatActivity{
 
         Button b = findViewById(R.id.save_button);
 
+        Intent i=getIntent();
+
+        final EditText et_name=findViewById(R.id.edit_user_name);
+        final EditText et_email=findViewById(R.id.edit_user_email);
+        final EditText et_location=findViewById(R.id.edit_user_location);
+        final EditText et_bio=findViewById(R.id.edit_user_bio);
+
+        if(i.getExtras()!=null){
+            String user_name=i.getExtras().get("user_name").toString();
+            String user_email=i.getExtras().get("user_email").toString();
+            String user_location=i.getExtras().get("user_location").toString();
+            String user_bio=i.getExtras().get("user_bio").toString();
+
+            et_name.setText(user_name);
+            et_email.setText(user_email);
+            et_location.setText(user_location);
+            et_bio.setText(user_bio);
+        }
+
         b.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
 
-
-
-                Intent i = new Intent(getApplicationContext(),EditProfile.class);
+                Intent i = new Intent(getApplicationContext(),ShowProfile.class);
+                i.putExtra("user_name", et_name.getText().toString() );
+                i.putExtra("user_email", et_email.getText().toString() );
+                i.putExtra("user_location", et_location.getText().toString() );
+                i.putExtra("user_bio", et_bio.getText().toString() );
                 startActivity(i);
+
             }
         });
 
--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
@@ -6,8 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class ShowProfile extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,38 @@ public class ShowProfile extends AppCompatActivity {
         setContentView(R.layout.activity_show_profile);
 
         Button button=findViewById(R.id.edit_button);
+
+        Intent i=getIntent();
+
+        final TextView tv_name=findViewById(R.id.user_name);
+        final TextView tv_email=findViewById(R.id.user_email);
+        final TextView tv_location=findViewById(R.id.user_location);
+        final TextView tv_bio=findViewById(R.id.user_bio);
+
+        if(i.getExtras()!=null){
+
+            String user_name=i.getExtras().get("user_name").toString();
+            String user_email=i.getExtras().get("user_email").toString();
+            String user_location=i.getExtras().get("user_location").toString();
+            String user_bio=i.getExtras().get("user_bio").toString();
+
+            tv_name.setText(user_name);
+            tv_email.setText(user_email);
+            tv_location.setText(user_location);
+            tv_bio.setText(user_bio);
+        }
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+
                 Intent i = new Intent(getApplicationContext(),EditProfile.class);
+                i.putExtra("user_name", tv_name.getText().toString() );
+                i.putExtra("user_email", tv_email.getText().toString() );
+                i.putExtra("user_location", tv_location.getText().toString() );
+                i.putExtra("user_bio", tv_bio.getText().toString() );
+
                 startActivity(i);
             }
         });
