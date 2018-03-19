d8acbfb13281eb1ed34bd08a2ca088529872ee8c
FrancescoPetro <37444709+FrancescoPetro@users.noreply.github.com>
2018-03-19 19:40:21 +0100

    Commit 19/03/18

--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/EditProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/EditProfile.java
@@ -6,22 +6,42 @@ package it.polito.mad.koko.lab1.kokolab1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static android.hardware.SensorManager.getOrientation;
 
 public class EditProfile extends AppCompatActivity{
 
+   /* private EditText et_name;
+    private EditText et_email;
+    private EditText et_location;
+    private EditText et_bio;
+*/
+    private static Bitmap Image = null;
+    private static Bitmap rotateImage = null;
+    private static final int GALLERY = 1;
+    private static Uri myImageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_edit_profile);
-
-        Button b = findViewById(R.id.save_button);
 
         Intent i=getIntent();
 
@@ -31,10 +51,10 @@ public class EditProfile extends AppCompatActivity{
         final EditText et_bio=findViewById(R.id.edit_user_bio);
 
         if(i.getExtras()!=null){
-            String user_name=i.getExtras().get("user_name").toString();
-            String user_email=i.getExtras().get("user_email").toString();
-            String user_location=i.getExtras().get("user_location").toString();
-            String user_bio=i.getExtras().get("user_bio").toString();
+            String user_name=i.getStringExtra("user_name");
+            String user_email=i.getStringExtra("user_email");
+            String user_location=i.getStringExtra("user_location");
+            String user_bio=i.getStringExtra("user_bio");
 
             et_name.setText(user_name);
             et_email.setText(user_email);
@@ -42,21 +62,75 @@ public class EditProfile extends AppCompatActivity{
             et_bio.setText(user_bio);
         }
 
-        b.setOnClickListener(new View.OnClickListener() {
+        ImageButton user_photo_button = findViewById(R.id.user_photo_button);
+        user_photo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Image != null)
+                    Image.recycle();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY);
+
+            }
+        });
+
+        Button save_button = findViewById(R.id.save_button);
+        save_button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
 
-                Intent i = new Intent(getApplicationContext(),ShowProfile.class);
+                Intent i = new Intent();
                 i.putExtra("user_name", et_name.getText().toString() );
                 i.putExtra("user_email", et_email.getText().toString() );
                 i.putExtra("user_location", et_location.getText().toString() );
                 i.putExtra("user_bio", et_bio.getText().toString() );
-                startActivity(i);
+                i.putExtra("uri", myImageUri.toString());
+                setResult(RESULT_OK, i);
+
+                finish();
 
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GALLERY && resultCode != 0) {
+            myImageUri = data.getData();
+            try {
+                Image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), myImageUri);
+                ImageView user_photo=findViewById(R.id.user_photo);
+                user_photo.setImageBitmap(Image);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+   /* @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("user_name",et_name.getText().toString());
+        outState.putString("user_email", et_email.getText().toString());
+        outState.putString("user_location", et_location.getText().toString());
+        outState.putString("user_bio", et_bio.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        et_name.setText(savedInstanceState.getString("user_name"));
+        et_email.setText(savedInstanceState.getString("user_email"));
+        et_location.setText(savedInstanceState.getString("user_location"));
+        et_bio.setText(savedInstanceState.getString("user_bio"));
+
+    }*/
 }
--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
@@ -1,16 +1,26 @@
 package it.polito.mad.koko.lab1.kokolab1;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class ShowProfile extends AppCompatActivity {
 
+   /* String saved_user_name;
+    String saved_user_email;
+    String saved_user_location;
+    String saved_user_bio;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,43 +30,137 @@ public class ShowProfile extends AppCompatActivity {
 
         Button button=findViewById(R.id.edit_button);
 
-        Intent i=getIntent();
+        final TextView tv_name;
+        final TextView tv_email;
+        final TextView tv_location;
+        final TextView tv_bio;
 
-        final TextView tv_name=findViewById(R.id.user_name);
-        final TextView tv_email=findViewById(R.id.user_email);
-        final TextView tv_location=findViewById(R.id.user_location);
-        final TextView tv_bio=findViewById(R.id.user_bio);
+        tv_name=findViewById(R.id.user_name);
+        tv_email=findViewById(R.id.user_email);
+        tv_location=findViewById(R.id.user_location);
+        tv_bio=findViewById(R.id.user_bio);
 
-        if(i.getExtras()!=null){
-
-            String user_name=i.getExtras().get("user_name").toString();
-            String user_email=i.getExtras().get("user_email").toString();
-            String user_location=i.getExtras().get("user_location").toString();
-            String user_bio=i.getExtras().get("user_bio").toString();
-
-            tv_name.setText(user_name);
-            tv_email.setText(user_email);
-            tv_location.setText(user_location);
-            tv_bio.setText(user_bio);
+        if(savedInstanceState!=null) {
+            tv_name.setText(savedInstanceState.getString("user_name"));
+            tv_email.setText(savedInstanceState.getString("user_email"));
+            tv_location.setText(savedInstanceState.getString("user_location"));
+            tv_bio.setText(savedInstanceState.getString("user_bio"));
         }
+
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
 
                 Intent i = new Intent(getApplicationContext(),EditProfile.class);
                 i.putExtra("user_name", tv_name.getText().toString() );
                 i.putExtra("user_email", tv_email.getText().toString() );
                 i.putExtra("user_location", tv_location.getText().toString() );
                 i.putExtra("user_bio", tv_bio.getText().toString() );
-
-                startActivity(i);
+                startActivityForResult(i,1);
             }
         });
 
+    }
+
+    /*@Override
+    protected void onPause() {
+        super.onPause();
+        Bundle outState=getIntent().getExtras();
+        TextView tv_name=findViewById(R.id.user_name);
+        TextView tv_email=findViewById(R.id.user_email);
+        TextView tv_location=findViewById(R.id.user_location);
+        TextView tv_bio=findViewById(R.id.user_bio);
+
+        outState.putString("user_name",tv_name.getText().toString());
+        outState.putString("user_email", tv_email.getText().toString());
+        outState.putString("user_location", tv_location.getText().toString());
+        outState.putString("user_bio", tv_bio.getText().toString());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        Bundle savedInstanceState=getIntent().getExtras();
+
+        TextView tv_name=findViewById(R.id.user_name);
+        TextView tv_email=findViewById(R.id.user_email);
+        TextView tv_location=findViewById(R.id.user_location);
+        TextView tv_bio=findViewById(R.id.user_bio);
+
+        tv_name.setText(savedInstanceState.getString("user_name"));
+        tv_email.setText(savedInstanceState.getString("user_email"));
+        tv_location.setText(savedInstanceState.getString("user_location"));
+        tv_bio.setText(savedInstanceState.getString("user_bio"));
+
+    }
+*/
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        TextView tv_name=findViewById(R.id.user_name);
+        TextView tv_email=findViewById(R.id.user_email);
+        TextView tv_location=findViewById(R.id.user_location);
+        TextView tv_bio=findViewById(R.id.user_bio);
+
+        outState.putString("user_name",tv_name.getText().toString());
+        outState.putString("user_email", tv_email.getText().toString());
+        outState.putString("user_location", tv_location.getText().toString());
+        outState.putString("user_bio", tv_bio.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        TextView tv_name=findViewById(R.id.user_name);
+        TextView tv_email=findViewById(R.id.user_email);
+        TextView tv_location=findViewById(R.id.user_location);
+        TextView tv_bio=findViewById(R.id.user_bio);
+
+        tv_name.setText(savedInstanceState.getString("user_name"));
+        tv_email.setText(savedInstanceState.getString("user_email"));
+        tv_location.setText(savedInstanceState.getString("user_location"));
+        tv_bio.setText(savedInstanceState.getString("user_bio"));
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode==1&&resultCode == RESULT_OK) {
+
+            String user_name=data.getStringExtra("user_name");
+            String user_email=data.getStringExtra("user_email");
+            String user_location=data.getStringExtra("user_location");
+            String user_bio=data.getStringExtra("user_bio");
+
+            TextView tv_name=findViewById(R.id.user_name);
+            TextView tv_email=findViewById(R.id.user_email);
+            TextView tv_location=findViewById(R.id.user_location);
+            TextView tv_bio=findViewById(R.id.user_bio);
+
+            tv_name.setText(user_name);
+            tv_email.setText(user_email);
+            tv_location.setText(user_location);
+            tv_bio.setText(user_bio);
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(data.getStringExtra("uri")));
+                ImageView user_photo=findViewById(R.id.user_photo);
+                user_photo.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
 }
