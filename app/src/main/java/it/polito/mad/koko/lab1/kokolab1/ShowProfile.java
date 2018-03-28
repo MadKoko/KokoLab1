e3ed1c141857cc4be50113a580433ce3f0281c6f
marcomicera <marco.micera@gmail.com>
2018-03-28 17:25:03 +0200

    Code review; Javadoc; deleting code bloat

--- app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
+++ app/src/main/java/it/polito/mad/koko/lab1/kokolab1/ShowProfile.java
@@ -14,102 +14,122 @@ import com.squareup.picasso.Picasso;
 
 public class ShowProfile extends AppCompatActivity {
 
-    private static String user_photo_uri=null;
+    /**
+     * User profile data.
+     */
+    private static String user_photo_uri = null;
     private TextView tv_name;
     private TextView tv_email;
     private TextView tv_location;
     private TextView tv_bio;
     private ImageView user_photo;
-    private String MY_PREFS_NAME="MySharedPreferences";
-
 
     /**
-     * ONCREATE() METHOD CREATES THE TEXT VIEWS AND SET THE CONTENT VIEW
-     *
+     * User profile data is stored in a shared XML file.
+     */
+    private String MY_PREFS_NAME = "MySharedPreferences";
+    private SharedPreferences sharedPreferences;
+
+    /**
+     * Instantiating the activity for the first time.
+     * @param savedInstanceState    activity's previously saved state.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Loading the XML layout file
         setContentView(R.layout.activity_show_profile);
 
+        // Restoring UI fields containing user info
         tv_name=findViewById(R.id.user_name);
         tv_email=findViewById(R.id.user_email);
         tv_location=findViewById(R.id.user_location);
         tv_bio=findViewById(R.id.user_bio);
-
         user_photo=findViewById(R.id.user_photo);
-
     }
 
     /**
-     * Ho inserito il menu nella barra superiore
-     * @param menu
-     * @return
+     * Setting the edit profile button in the title bar.
+     * @param menu      menu object to be instantiated.
+     * @return          whether the menu object has been inserted or not.
      */
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
+    /**
+     * Handling the menu item selection.
+     * @param item      item that has been selected.
+     * @return          whether the menu item has been handled successfully.
+     */
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        switch(item.getItemId()) {
+
+            // Profile editing
             case R.id.edit:
                 Intent i = new Intent(getApplicationContext(), EditProfile.class);
-                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(MY_PREFS_NAME,MODE_PRIVATE);
+
                 SharedPreferences.Editor editor = sharedPreferences.edit();
 
-                if (user_photo_uri != null) {
+                // If there previously was a profile pic
+                if(user_photo_uri != null)
+                    // Restore it
                     editor.putString("user_photo_temp", user_photo_uri);
-                }
-                else{
+                else
+                    // Default profile pic
                     editor.putString("user_photo_temp", String.valueOf(R.mipmap.ic_launcher_round));
-                }
+
+                // Writing in the sharedPreferences data structure containing user info
                 editor.apply();
+
+                // Launching the editing profile activity
                 startActivity(i);
-               // startActivityForResult(i, 1);
+
             default:
                 return super.onOptionsItemSelected(item);
 
         }
     }
 
-
     /**
-     * ONRESUME() METHOD SET ALL THE FIELDS WITH VALUES TAKEN FROM SHARED PREFERENCES
+     * Filling all the UI fields retrieving all the nedded information from the
+     * sharedPreferences XML file.
      */
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(MY_PREFS_NAME,MODE_PRIVATE);
 
+        // Updating the sharedPreferences data structure containing user info
+        sharedPreferences = getApplicationContext().getSharedPreferences(MY_PREFS_NAME,MODE_PRIVATE);
+
+        // Filling UI elements
         tv_name.setText(sharedPreferences.getString("user_name",null));
         tv_email.setText(sharedPreferences.getString("user_email",null));
         tv_location.setText(sharedPreferences.getString("user_location",null));
         tv_bio.setText(sharedPreferences.getString("user_bio",null));
 
-        if(sharedPreferences.getString("user_photo",null)==null){
-            Picasso.get().load(R.mipmap.ic_launcher_round).fit().centerCrop().into(user_photo);
-        }
 
+        if(sharedPreferences.getString("user_photo",null) == null)
+            Picasso.get().load(R.mipmap.ic_launcher_round).fit().centerCrop().into(user_photo);
         else {
-            user_photo_uri=sharedPreferences.getString("user_photo", null);
+            user_photo_uri = sharedPreferences.getString("user_photo", null);
             Picasso.get().load(sharedPreferences.getString("user_photo", null)).fit().centerCrop().into(user_photo);
         }
-
-
     }
 
     /**
      * ONPAUSE() METHOD SAVES ALL THE CURRENT VALUES IN FIELDS IN SHARED PREFERENCES
      */
-
-   @Override
+    /*@Override
     protected void onPause() {
         super.onPause();
 
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(MY_PREFS_NAME,MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         editor.putString("user_name", tv_name.getText().toString());
@@ -120,7 +140,6 @@ public class ShowProfile extends AppCompatActivity {
         if(user_photo_uri!=null)
             editor.putString("user_photo",user_photo_uri);
         editor.apply();
-
-    }
+    }*/
 
 }
